5403827a771c02e5aa04025837c462ca026ac7b0
Wes Burningham <wburningham@gmail.com>
2026-04-10 09:35:02 -0600

    fix: fix terminal characters

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,8 +3,11 @@ fish_config theme choose "catppuccin-mocha"
 # set -U -e tide_right_prompt_items
 # Not sure, but this is needed to remove the items
 # set -U -e _tide_right_items
-# Force default char (broke in v4.3.0+)
-set -Ux tide_character_vi_icon_default '❯'
+set -g tide_character_vi_icon_default '❯'
+set -g tide_character_vi_icon_normal  '❯'
+set -g tide_character_vi_icon_visual  '❯'
+set -g tide_character_vi_icon_replace '❯'
+
 
 # === secrets ===
 # Source secrets not committed to VCS
