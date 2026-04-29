dce8c97fcc07f8ba5216b334725716c1c518fcc0
Wes Burningham <wburningham@gmail.com>
2026-04-29 10:36:23 -0600

    feat(nvim): add support for cmd+up/down

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -17,6 +17,7 @@ vim.lsp.config("typos_lsp", {
 vim.lsp.enable("typos_lsp")
 
 -- Insert mode: jump to start / end of line (C-a / C-e: Zed terminal + Ghostty cmd-arrows).
+-- Cmd+up / cmd+down: Zed keymap + Ghostty send Esc+gg / Esc+G (no Lua here; works in normal + insert).
 -- Re-add <Home>/<End> imaps if you want Fn+arrows or a real Home/End key in other terminals.
 vim.keymap.set("i", "<C-a>", "<C-o>0", { desc = "Beginning of line (C-a, Zed/Ghostty cmd-arrows)" })
 vim.keymap.set("i", "<C-e>", "<C-o>$", { desc = "End of line (C-e, Zed/Ghostty cmd-arrows)" })
